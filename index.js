73514cfee1eb0652c3f890932644dda0e4f6bde0
Gabriel <gabrielorgo5@gmail.com>
2021-10-08 23:45:29 -0300

    selecting a render processor

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require("express")(); // O express retorna uma função, portanto fazendo assim iremos encurtar o código
 
-var port = 3000 || process.env.port
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) =>{
-    res.send("<html><body>Ay</body></html>")
-})
+var port = 3000 || process.env.port;
 
-app.listen(port, console.log(`Rodando na porta ${port}`))
+app.get("/", (req, res) => {
+    res.send("<html><body>Ay</body></html>");
+});
+
+app.get("/tecnologia", (req, res) => {
+    res.render("secao/tecnologia"); // Este é o caminho até a view
+});
+
+app.listen(port, console.log(`Rodando na porta ${port}`));
