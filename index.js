bfe27bb1de24d596ad1b8e1ac5457d2a823e0970
Gabriel <gabrielorgo5@gmail.com>
2021-10-09 19:12:55 -0300

    refactoring views

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var app = require("express")(); // O express retorna uma função, portanto fazendo assim iremos encurtar o código
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // Especifíca o tipo de arquivo que será usado como template engine
 
 var port = 3000 || process.env.port;
 
 app.get("/", (req, res) => {
-    res.send("<html><body>Ay</body></html>");
+    res.render("home/index"); // Este é o caminho até a view
 });
 
-app.get("/tecnologia", (req, res) => {
-    res.render("secao/tecnologia"); // Este é o caminho até a view
+app.get("/formulario_inclusao_noticia", (req, res) => {
+    res.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", (req, res) => {
+    res.render("noticias/noticias");
 });
 
 app.listen(port, console.log(`Rodando na porta ${port}`));
