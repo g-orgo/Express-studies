a7041f381db891c771bbf2419155e85f558e41c6
Gabriel <gabrielorgo5@gmail.com>
2021-10-14 00:42:26 -0300

    creating frst db objects

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,25 @@
 module.exports = function (app) {
     app.get("/noticias", (req, res) => {
-        res.render("noticias/noticias");
+        var mysql = require("mysql");
+
+        var connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "root",
+            database: "portal_noticias",
+        });
+        connection.connect();
+
+        connection.query(
+            "SELECT * FROM noticias",
+            function (err, result) {
+                /* query() espera como parâmetros uma
+                chamada em SQL para o banco e uma função
+                callback contendo erro e resultado */
+                if (err) throw err;
+                res.render("noticias/noticias", {news: result}); // Passando as informações do banco para o front
+            }
+        );
+
     });
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require("./config/server");
 
-var newsRoute = require("./app/routes/noticias")(app);
+var newsRoute = require("./app/routes/noticias")(app); // Passando a aplicação para a rota
 var homeRoute = require("./app/routes/home")(app);
 var addNewsRoute = require("./app/routes/formulario_inclusao_noticia")(app);
